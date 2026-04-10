81ab3247c9119f991024a3ce6838a9a2c0329d2f
Christian <christian.bankovic@gmail.com>
2026-04-09 20:14:17 -0500

    all note spells in, next do sounds

--- files/scripts/gun/gun_actions.lua
+++ files/scripts/gun/gun_actions.lua
@@ -1,59 +1,25 @@
 dofile_once("data/scripts/gun/procedural/gun_action_utils.lua")
 dofile_once("data/scripts/lib/utilities.lua")
-local MOD_ID = "NOTES_EXPANDED"
-local mod_id = MOD_ID:lower()
-local KANTELE = "KANTELE_"
-local kantele = KANTELE:lower()
-local OCARINA = "OCARINA_"
-local ocarina = OCARINA:lower()
-local new_sounds_of_music = {
-	{
-		id          = MOD_ID.."_KANTELE_C",
-		name 		= "$action_notes_expanded_kantele_c",
-		description = "$actiondesc_notes_expanded_generic_note",
-		spawn_requires_flag = "card_unlocked_kantele",
-		sprite 		= "mods/NotesExpanded/files/ui_gfx/gun_actions/kantele_c.png",
-		related_projectiles	= {"mods/NotesExpanded/files/entities/projectiles/deck/kantele/kantele_c.xml"},
-		type 		= ACTION_TYPE_OTHER,
-		spawn_level                       = "10", -- OCARINA_D
-		spawn_probability                 = "0", -- OCARINA_D
-		price = 10,
-		mana = 1,
-		action 		= function()
-			add_projectile("mods/NotesExpanded/files/entities/projectiles/deck/kantele/kantele_c.xml")
-			c.fire_rate_wait = c.fire_rate_wait + 15
-		end,
-	},
-}
+local mod_id = "NOTES_EXPANDED"
+local new_sounds_of_music = {}
+local music_data = dofile_once("mods/NotesExpanded/files/scripts/misc/music_data.lua")
 
-local music_data = {
-    {id = "kantele_c", image = "kantele_c"},
-    {id = "kantele_csharp", image = "kantele_c", flat = "kantele_dflat"}, -- bad image
-    -- {id = "kantele_dsharp", flat = "kantele_eflat"}, -- already made
-    {id = "kantele_f", image = "kantele_f"},
-    {id = "kantele_fsharp", image = "kantele_c", flat = "kantele_gflat"}, -- bad image
-    {id = "kantele_gsharp", image = "kantele_gsharp",
-    flat = "kantele_aflat"},
-    {id = "kantele_asharp", image = "kantele_c", flat = "kantele_bflat"}, -- bad image
-    {id = "kantele_b", image = "kantele_b"},
-    {id = "kantele_c2", image = "kantele_c"}, -- bad image
-}
 
 for i,data in ipairs(music_data) do
     local note = {
-		id                  = MOD_ID.."_"..data.id:upper(),
+		id                  = mod_id.."_"..data.id:upper(),
 		name 		        = "$action_notes_expanded_"..data.id,
 		description         = "$actiondesc_notes_expanded_generic_note",
-		spawn_requires_flag = "card_unlocked_kantele",
-		sprite 		        = ("mods/NotesExpanded/files" and data.image == nil or "data").."/ui_gfx/gun_actions/"..(data.id and data.image == nil or data.image)..".png",
-		related_projectiles	= {"mods/NotesExpanded/files/entities/projectiles/deck/kantele/"..data.id..".xml"},
+		spawn_requires_flag = "card_unlocked_"..(data.kantele and "kantele" or "ocarina"),
+		sprite 		        = ((not data.vanilla_image) and "mods/NotesExpanded/files" or "data").."/ui_gfx/gun_actions/"..data.id..".png",
+		related_projectiles	= {"mods/NotesExpanded/files/entities/projectiles/deck/"..(data.kantele and "kantele" or "ocarina").."/"..data.id..".xml"},
 		type 		        = ACTION_TYPE_OTHER,
 		spawn_level         = "10",
 		spawn_probability   = "0",
 		price               = 10,
 		mana                = 1,
 		action 		        = function()
-		    	                add_projectile("mods/NotesExpanded/files/entities/projectiles/deck/kantele/"..data.id..".xml")
+		    	                add_projectile("mods/NotesExpanded/files/entities/projectiles/deck/"..(data.kantele and "kantele" or "ocarina").."/"..data.id..".xml")
 		    	                c.fire_rate_wait = c.fire_rate_wait + 15
 		                    end,
         author              = "Sharpy796",
---
+++ files/scripts/misc/music_data.lua
@@ -0,0 +1,21 @@
+local music_data = {
+    {kantele = true, id = "kantele_c", vanilla_image = true},
+    {kantele = true, id = "kantele_csharp", flat = "kantele_dflat"},
+    -- {id = "kantele_dsharp", flat = "kantele_eflat"}, -- already made
+    {kantele = true, id = "kantele_f", vanilla_image = true},
+    {kantele = true, id = "kantele_fsharp", flat = "kantele_gflat"},
+    {kantele = true, id = "kantele_gsharp", flat = "kantele_aflat", vanilla_image = true},
+    {kantele = true, id = "kantele_asharp", flat = "kantele_bflat"},
+    {kantele = true, id = "kantele_b", vanilla_image = true},
+    {kantele = true, id = "kantele_c2"},
+
+	{kantele = false, id = "ocarina_csharp", flat = "ocarina_dflat"},
+	{kantele = false, id = "ocarina_dsharp", flat = "ocarina_eflat"},
+	{kantele = false, id = "ocarina_fsharp", flat = "ocarina_gflat"},
+	{kantele = false, id = "ocarina_g"},
+	-- {kantele = false, id = "ocarina_gsharp", flat = "ocarina_aflat"}, -- already made
+	{kantele = false, id = "ocarina_asharp", flat = "ocarina_bflat"},
+	{kantele = false, id = "ocarina_c2"},
+}
+
+return music_data
